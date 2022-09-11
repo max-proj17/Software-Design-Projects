86d004fef2f79265a620ec2c0b884e469410f64d
mfinch1 <maximelio-finch@uiowa.edu>
2022-09-10 21:41:44 -0500

    Declared class functions (exchangeValid(), usdToSWD(), exchangeSWD(), setRate())for ExchangeSystem

--- oral_exam1/B01_ExchangeComputation/src/ExchangeSystem.java
+++ oral_exam1/B01_ExchangeComputation/src/ExchangeSystem.java
@@ -1,2 +1,21 @@
 public class ExchangeSystem {
+
+    private static float rate = 1; //default rate is 1 because a rate of 0 would return 0 SWD
+
+    ExchangeSystem(float rate){
+        this.rate = rate;
+
+    }
+
+    private boolean exchangeValid(){return true;}
+    private static float usdToSWD(){
+        return 0.0f;
+    }
+    public void exchangeSWD(){}
+
+    public void setRate(float rate)
+    {
+        this.rate = rate;
+    }
+
 }
