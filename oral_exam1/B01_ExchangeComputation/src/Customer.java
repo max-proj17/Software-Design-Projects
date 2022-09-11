60f25b70e7b67c60b30e834a9c7737d909a32b1b
mfinch1 <maximelio-finch@uiowa.edu>
2022-09-10 20:53:39 -0500

    Added balance variable and getBalance() function to Customer class. Created the ExchangeSystem class.

--- oral_exam1/B01_ExchangeComputation/src/Customer.java
+++ oral_exam1/B01_ExchangeComputation/src/Customer.java
@@ -1,2 +1,13 @@
 public class Customer {
+
+    //float precision is used because double precision will result in an off by one error
+    private float balance;
+
+    Customer(float startBalance){
+        balance = startBalance;
+    }
+    public float getBalance() //not static because all Customer's have a different balance
+    {
+        return balance;
+    }
 }
