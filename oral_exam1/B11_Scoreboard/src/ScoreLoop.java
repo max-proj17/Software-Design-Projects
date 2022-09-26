524ca9d7fdeef7b0d3ca15f43ac063350b989f69
mfinch1 <maximelio-finch@uiowa.edu>
2022-09-26 16:18:50 -0500

    - Implemented a basic menu screen with FURTHER input validation. Game/Team objects not yet created - String homeTeam and String awayTeam will not be instance variables of ScoreLoop for long. They are just test variables. The constructor for a not yet built Game object will take these names in as final strings.

--- oral_exam1/B11_Scoreboard/src/ScoreLoop.java
+++ oral_exam1/B11_Scoreboard/src/ScoreLoop.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
 
 public class ScoreLoop {
+    private Scanner sc = new Scanner(System.in);
+    private int gameOption = 0;
+    private String homeTeam;
+    private String awayTeam;
     public ScoreLoop()
     {
         menuScreen();
     }
-    private static void menuScreen()
+    private void menuScreen()
     {
-        int gameOption;
-        Boolean valid = false;
-        Scanner sc = new Scanner(System.in);
+        String homeTeam;
+        String awayTeam;
+
         System.out.println("Select the game you would like to play");
         System.out.println("1. Soccer");
         System.out.println("2. Basketball");
@@ -17,17 +21,56 @@ public class ScoreLoop {
         System.out.println("4. Football");
         System.out.println("5. Hockey");
 
-        do {
-            System.out.println("Enter Choice: ");
-            if (sc.hasNextInt()){
-                gameOption = sc.nextInt();
-                valid = true;
-            }else {
+        validInput(1);
 
-                System.out.println("Invalid Input");
-                sc.nextLine();
-            }
-        }while(!valid);
+        System.out.println("Enter Home Team: ");
+        validInput(2);
+        System.out.println("Enter Away Team: ");
+        validInput(2);
+
+    }
+    private void validInput(int inputType)
+    {
+        //Input type 1: Game selection validation
+        //Input type 2: Team name validation
+        //Input type 3: Game choice validation
+        Boolean valid = false;
+        if(inputType == 1)
+        {
+            do {
+                System.out.println("Enter Choice: ");
+                if (sc.hasNextInt()){
+                    //We must parse the int from the whole line because the scanner cursor will stop at the int and not automatically go to a new line.
+                    int tmp = Integer.parseInt(sc.nextLine());
+                    if(tmp== 1 || tmp == 2 || tmp==3 || tmp==4 || tmp==5)
+                    {
+                        gameOption = tmp;
+                        valid = true;
+
+                    }else {
+                        System.out.println("Invalid Input");
+                        sc.nextLine();
+                    }
+                }else{
+
+                    System.out.println("Invalid Input");
+                    sc.nextLine();
+                }
+            }while(!valid);
+        }else if(inputType == 2)
+        {
+            do {
+                if (!sc.hasNextInt()){
+                    valid = true;
+                    homeTeam = sc.nextLine();
+
+                }else{
+
+                    System.out.println("Invalid Input. Team name must contain letters.");
+                    sc.nextLine();
+                }
+            }while(!valid);
+        }
     }
 
 }
