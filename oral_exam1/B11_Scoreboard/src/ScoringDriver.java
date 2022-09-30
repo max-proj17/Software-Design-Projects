6e5bcb29e987bf580c43a11d33e852879ff5a91d
mfinch1 <maximelio-finch@uiowa.edu>
2022-09-29 23:42:10 -0500

    - Added another Javadocs comment for ScoringDriver.

--- oral_exam1/B11_Scoreboard/src/ScoringDriver.java
+++ oral_exam1/B11_Scoreboard/src/ScoringDriver.java
@@ -3,6 +3,12 @@
  * @author Max Finch
  */
 public final class ScoringDriver {
+    /**
+     * This method will automatically be called when ScoringDriver
+     * is run.
+     *
+     * @param args Command line arguments.
+     */
     public static void main (String [] args)
     {
         ScoreLoop scoreloop = new ScoreLoop();
