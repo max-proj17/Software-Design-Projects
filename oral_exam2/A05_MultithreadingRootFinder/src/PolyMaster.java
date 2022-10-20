274d1474e92975ef4c0f0c70b951a9845c0e1498
mfinch1 <maximelio-finch@uiowa.edu>
2022-10-19 21:04:37 -0500

    Finished the main calculation needed for this assignment. I am still confused on how to structure the classes because the assignment description is a little confusing.

---
+++ oral_exam2/A05_MultithreadingRootFinder/src/PolyMaster.java
@@ -0,0 +1,7 @@
+public class PolyMaster {
+
+    public static void main (String [] args)
+    {
+
+    }
+}
---
+++ oral_exam2/A05_MultithreadingRootFinder/src/PolyServant.java
@@ -0,0 +1,18 @@
+public class PolyServant implements Runnable{
+
+    private final double[] coefficients;
+    private int processed;
+    public PolyServant(double [] coefficients) //replace this array with proper buffer object later
+    {
+        this.coefficients = coefficients;
+    }
+    @Override
+    public void run() {
+
+        double real = -coefficients[1] / (2 * coefficients[0]);
+        double img = Math.sqrt(-(coefficients[1] * coefficients[1] - 4 * coefficients[0] * coefficients[2])) / ( 2 * coefficients[0]);
+
+
+
+    }
+}
