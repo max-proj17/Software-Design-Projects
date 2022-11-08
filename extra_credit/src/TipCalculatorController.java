dae304cd4c24c7591892fcf443f96c7ab3b46dac
mfinch1 <maximelio-finch@uiowa.edu>
2022-11-08 10:48:11 -0600

    started tip calculator extra credit

---
+++ extra_credit/src/TipCalculatorController.java
@@ -0,0 +1,74 @@
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+
+public class TipCalculatorController {
+
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percent = NumberFormat.getPercentInstance();
+
+    private BigDecimal tipPercentage = new BigDecimal(0.15);
+
+    @FXML
+    private Label tipPercentageLabel;
+
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private TextField tipTextField;
+
+    @FXML
+    private TextField totalTextField;
+
+    @FXML
+    private Slider tipPercentageSlider;
+
+    @FXML
+    private void calculateButtonPressed(ActionEvent event)
+    {
+        try
+        {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        }catch (NumberFormatException ex)
+        {
+            amountTextField.setText("Enter Amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
+
+    public void intialize()
+    {
+        currency.setRoundingMode(RoundingMode.HALF_UP);
+
+
+        tipPercentageSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+
+                        tipPercentage = BigDecimal.valueOf(newValue.intValue()/100.0);
+                        tipPercentageLabel.setText(percent.format(tipPercentage));
+                    }
+                }
+
+
+        );
+    }
+
+
+
+}
