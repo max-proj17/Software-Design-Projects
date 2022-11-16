c6443c4b71c63bb2dc1bbfc130e4cf2c7f230918
mfinch1 <maximelio-finch@uiowa.edu>
2022-11-16 16:35:18 -0600

    Finished both Encoder and Decoder logic. Must add input validation now.

--- oral_exam2/A15_Barcodes/src/UPCADecoder.java
+++ oral_exam2/A15_Barcodes/src/UPCADecoder.java
@@ -1,2 +1,55 @@
+import java.util.HashMap;
+
 public class UPCADecoder {
+    //User input check
+    //For now just require input to be spaced. We can change later.
+    //Loop through input without 101's and middle and get integer.
+    //Get check digit at end.
+
+    final static HashMap<String, Integer> upc_w_b = new HashMap<>();
+    final static HashMap<String, Integer> upc_b_w = new HashMap<>();
+
+
+    private static void setup()
+    {
+        upc_w_b.put("0001101", 0); upc_w_b.put("0011001", 1); upc_w_b.put("0010011", 2); upc_w_b.put("0111101", 3); upc_w_b.put("0100011", 4);
+        upc_w_b.put("0110001", 5); upc_w_b.put("0101111", 6); upc_w_b.put("0111011", 7); upc_w_b.put("0110111", 8); upc_w_b.put("0001011", 9);
+
+        upc_b_w.put("1110010", 0); upc_b_w.put("1100110", 1); upc_b_w.put("1101100", 2); upc_b_w.put("1000010", 3); upc_b_w.put("1011100", 4);
+        upc_b_w.put("1001110", 5); upc_b_w.put("1010000", 6); upc_b_w.put("1000100", 7); upc_b_w.put("1001000", 8); upc_b_w.put("1110100", 9);
+
+    }
+    public static String decode(String input)
+    {
+        setup();
+        StringBuilder returnStr = new StringBuilder();
+
+        //sliding window
+        int i;
+        //i 0,1,2 is "101"
+        //i 3-50 is 7 digit code
+        //i 46-27 is middle
+        //i 28-
+        for(i=3; i<=38; i+=7)
+        {
+          returnStr.append(upc_w_b.get(input.substring(i, i+7)));
+        }
+        for (i=50; i<=78; i+=7)
+        {
+            returnStr.append(upc_b_w.get(input.substring(i, i+7)));
+        }
+
+
+        int checkDigit = upc_b_w.get(input.substring(i, i+7));
+        return "Check digit: " + checkDigit + "" +
+                "\nProduct code: " + returnStr;
+    }
+
+    public static String inputValidation()
+    {
+        return "";
+    }
+
+
+
 }
--- oral_exam2/A15_Barcodes/src/UPCAEncoder.java
+++ oral_exam2/A15_Barcodes/src/UPCAEncoder.java
@@ -1,2 +1,59 @@
+import java.util.HashMap;
+
 public class UPCAEncoder {
+
+    //Use hashmap to get String equivalent. 101 + (6)w-b-w-b + 01010 + (6)b-w-b-w + 101
+    final static HashMap<Integer, String> upc_w_b = new HashMap<>();
+    final static HashMap<Integer, String> upc_b_w = new HashMap<>();
+
+    private static void setup()
+    {
+        upc_w_b.put(0, "0001101"); upc_w_b.put(1, "0011001"); upc_w_b.put(2, "0010011"); upc_w_b.put(3, "0111101"); upc_w_b.put(4, "0100011");
+        upc_w_b.put(5, "0110001"); upc_w_b.put(6, "0101111"); upc_w_b.put(7, "0111011"); upc_w_b.put(8, "0110111"); upc_w_b.put(9, "0001011");
+
+        upc_b_w.put(0, "1110010"); upc_b_w.put(1, "1100110"); upc_b_w.put(2, "1101100"); upc_b_w.put(3, "1000010"); upc_b_w.put(4, "1011100");
+        upc_b_w.put(5, "1001110"); upc_b_w.put(6, "1010000"); upc_b_w.put(7, "1000100"); upc_b_w.put(8, "1001000"); upc_b_w.put(9, "1110100");
+
+    }
+    private static int checkDigitCreator(int[] nums) {
+        int odd_digits = nums[0] + nums[2] + nums[4] + nums[6] + nums[8] + nums[10];
+        int even_digits = nums[1] + nums[3] + nums[5] + nums[7] + nums[9];
+
+        int result = (3 * (odd_digits) + even_digits) % 10;
+        if(result != 0)
+        {
+            result = 10 - result;
+        }
+        return result;
+
+    }
+    public static String encode(String input)
+    {
+        setup();
+        int[] nums = new int[12];
+        StringBuilder returnStr = new StringBuilder();
+        for(int i=0; i<input.length(); i++)
+        {
+            nums[i] = Character.getNumericValue(input.charAt(i));
+        }
+        nums[11] = checkDigitCreator(nums);
+        returnStr.append("101");
+        for (int i=0; i<6; i++)
+        {
+            returnStr.append(upc_w_b.get(nums[i]));
+        }
+        returnStr.append("01010");
+        for (int i=6; i<12; i++)
+        {
+            returnStr.append(upc_b_w.get(nums[i]));
+        }
+        returnStr.append("101");
+        return returnStr.toString();
+    }
+    public static String inputValidation()
+    {
+        return "";
+    }
+
+
 }
