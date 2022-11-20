ccb6319bd79e60f632d74961a9a703e1ba3d42be
mfinch1 <maximelio-finch@uiowa.edu>
2022-11-20 16:35:13 -0600

    Added temporary mouse listener to get coordinates of US map.

--- oral_exam2/B12_ElectoralCollege/src/ElectoralCollege.java
+++ oral_exam2/B12_ElectoralCollege/src/ElectoralCollege.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,7 +18,15 @@ public class ElectoralCollege extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("electoralcollegefxml.fxml")));
-
+        root.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getEventType() == MouseEvent.MOUSE_MOVED)
+                {
+                    System.out.println(mouseEvent.getSceneX() + ", " + mouseEvent.getSceneY());
+                }
+            }
+        });
         Scene scene = new Scene (root);
         stage.setTitle("Electoral College Application");
         stage.setScene(scene);
